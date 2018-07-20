8e577bbfc8546436ef6bf66f0bc2c7dcabd17804
Cemal Biryol <cbbiryol@Cemals-MacBook-1774.local>
2018-07-20 14:55:42 -0400

    Server with authentication-1

---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+/** Default import for all routes under the api folder */
+//Node level imports
+const express = require('express')
+
+module.exports = function(){
+    //Create a Router instance, so we can mount routes on that and pass it up higher in the imports.
+    const Router = express.Router()
+    //Mount the Route object generated and imported from test.js on this Router object.
+    Router.use(require('./test')())
+    //Mount the Route object generated and imported from user.js on this Router object.
+    Router.use(require('./user')())
+    //Return the Router, since this has all the routes mounted on it and we have to user it higher up.
+    return Router
+}
