51ae3951d4cb64e887b728c24123860df3f2329c
Edward Nimocks <enimocks@gmail.com>
2018-07-22 00:32:19 -0400

    reimplement navbar/pic overlap

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
-  <nav className="navbar navbar-expand-lg">
+  <nav className="navbar navbar-expand-lg fixed-top">
     <Link className="navbar-brand" to="/">
       CrankHeads
     </Link>
