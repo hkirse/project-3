9f72811c8ff3a87e3bce33ffa1c86c096b47afcf
Edward Nimocks <enimocks@gmail.com>
2018-07-23 19:06:39 -0400

    connect page API fetch working

---
+++ client/src/components/EventCard/EventCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EventCard.css';
+
+const EventCard = props => (
+  <div class="card">
+    <div class="card-body">
+      This is some text within a card body.
+    </div>
+  </div>
+);
+
+export default EventCard;
--- client/src/pages/Connect/Connect.js
+++ client/src/pages/Connect/Connect.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
+//FIXME:import EventCard from '../../components/EventCard';
 
 // const API_KEY = "7dc5e202b7b3ee7419e3d42654353"; ***obtained API key from website***
-
-// code from "https://reactjs.org/docs/faq-ajax.html"
 
 class Connect extends Component {
   constructor(props) {
@@ -15,46 +14,46 @@ class Connect extends Component {
   }
 
   componentDidMount() {
-    fetch("https://api.meetup.com/torc-nc/events?photo-host=public&page=20&sig_id=259358047&sig=c03cc0afa57c58c7b2c36569d72c10144a3bf263")
+    fetch("https://api.meetup.com/torc-nc/events?photo-host=public&page=20&sig_id=259358047&sig=01606d3709043fff8d5efc612870ea93595c5177")
       .then(res => res.json())
-      .then(json => console.log(json))
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            events: result.events
-          });
-          console.log(result.events);
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+      .then((result) => {
+        this.setState({
+          error: null,
+          isLoaded: true,
+          events: result
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          events:[],
+          isLoaded: true,
+          error
+        });
+      })
   }
 
   render() {
     const { error, isLoaded, events } = this.state;
-
+    // Log it out!
+    console.log(events);
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
-          {events.map(event => (
-            <li key={event.name}>
-              {event.name}
-            </li>
-          ))}
-        </ul>
+        // <EventCard
+          <ul>
+            {events.map(event => (
+              <li key={event.id}>
+                {event.name}
+              </li>
+            ))}
+          </ul>
+        // />
       );
     }
   }
 }
-// end code from "https://reactjs.org/docs/faq-ajax.html"
 
 export default Connect;
