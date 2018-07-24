0f0896bf5be11849481004db266edf390b922ce1
Edward Nimocks <enimocks@gmail.com>
2018-07-24 16:17:20 -0400

    event page fix

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -19,7 +19,7 @@ const EventCard = props => (
         </div>
         <div className="col-md-4">
           <img className="my-3" src={props.image} alt="meetup" />
-          <h5 className="card-text" style={{fontWeight: "bold"}}><i class="fa fa-map-marker" aria-hidden="true"></i> {props.venue_name}</h5>
+          <h5 className="card-text" style={{fontWeight: "bold"}}><i className="fa fa-map-marker" aria-hidden="true"></i> {props.venue_name}</h5>
           <p className="card-text" style={{fontSize: "1.05rem"}}>{props.venue_street} • {props.venue_city}, {props.venue_state}</p>
           <p className="card-text" style={{fontSize: "1.05rem"}}>{props.rsvpcount} people are going to this event!</p>
           <a href={props.link} style={{color: "white"}} role="button" className="my-3 btn btn-lg btn-block btn-info">Go to event page</a>
