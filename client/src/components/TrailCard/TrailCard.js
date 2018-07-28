cafbab64016cffc0c67cece727d349bf102d706f
Cemal Biryol <cbbiryol@Cemals-MacBook-1774.local>
2018-07-28 13:23:10 -0400

    INcorporated

--- client/src/components/TrailCard/TrailCard.js
+++ client/src/components/TrailCard/TrailCard.js
@@ -4,7 +4,7 @@ import { Button } from 'reactstrap';
 
 
 const TrailCard = props => (
-  <div className="card">
+  <div className="card cardTrail">
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
